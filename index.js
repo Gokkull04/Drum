650fcaa94349b6654f3faa907b33cf83482a9f3a
Gokulll15 <gokkull04@gmail.com>
2023-10-24 19:03:03 +0530

    Fc

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 var totalImages = document.querySelectorAll(".drum").length;
 for(var i = 0;i < totalImages; i++){
 document.querySelectorAll("button")[i].addEventListener("click", function() {
-var audio = new Audio("sounds/tom-1.mp3");
-audio.play();
+var buttonInnerHTML = this.innerHTML;
+switch (buttonInnerHTML) {
+    case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+    case "a":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+    case "s":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+    case "d":
+        var audio = new Audio("sounds/tom-4.mp3");
+        audio.play();
+        break;
+    case "j":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+    case "k":
+        var audio = new Audio("sounds/kick-bass.mp3");
+        audio.play();
+        break;
+    case "l":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+    default:
+        console.log(buttonInnerHTML);
+        break;
+}
 })};
